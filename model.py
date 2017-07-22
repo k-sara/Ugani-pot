2728d38c4d8c4bdc6131e71f3e3482b091ac66a9
Sara Kačarević <sara.kacarevic@student.fmf.uni-lj.si>
2017-07-22 22:30:31 +0200

    Dodan gumb za novo igro, števec poskusov, okno za vpis imena ter avtomatično shranjevanje rezultata ob koncu igre

--- model.py
+++ model.py
@@ -11,17 +11,17 @@ class Polje:
     def __init__(self, visina=10, sirina=10):
         self.visina = visina
         self.sirina = sirina
-        self.polja = [] 
-        self.korak = 1
+        self.korak = 0
         self.seznam = []
         self.resitev = []
+        self.stevec = 0
         
         
     def nova_igra(self):
         # Ob začetku igre ustvari pot (rešitev)
         self.seznam = 9*[DESNO] + 9*[GOR]
         random.shuffle(self.seznam)
-        self.resitev = [(self.visina -1, 0)]
+        self.resitev = []
         i, j = (self.visina - 1, 0)
         for smer in self.seznam:
             if smer == GOR:
@@ -31,40 +31,22 @@ class Polje:
             self.resitev.append((i,j))
 
 
-
     def ugibaj(self, smer):
-        if self.korak == 18:
-            return None
-        #Preveri če si zadel pot ali ne
-        #i, j = self.resitev[self.korak]
+        #Preveri če si zadel pot ali ne, tako da primerja
+        #pritisnjeno tipko s seznamom rešitve
+        
         if smer == self.seznam[self.korak]:
             self.korak += 1
-
         else:
             #zgrešiš
-            self.korak = 1
+            self.korak = 0
+            self.stevec += 1
             
-        
-    
 
     def __repr__(self):
         return 'Polje(visina={}, sirina={}, resitev={})'.format(
             self.visina, self.sirina, self.resitev)
 
-#    def __str__(self):
 
-
-#        for _ in range(self.visina):
-#           self.polja.append(self.sirina * [' '])
-
-#        START = self.polja[self.visina - 1][0] = 'S'
-#        CILJ = self.polja[0][self.sirina - 1] = 'C'
-
-            
-#        niz = ''
-#        rob = '+' + self.sirina * '-' + '+\n'
-#        for vrstica in self.polja:
-#            niz += '|'+ ''.join(vrstica) + '|\n'
-#        return rob + niz + rob
 
 
--- ugani_pot.py
+++ ugani_pot.py
@@ -4,36 +4,96 @@ import model
 class Pot:
     def __init__(self, okno):
         self.polje = model.Polje(10, 10)
-
-        self.okvir = tk.Frame(okno, height=200, width=200)
-        self.okvir.place(x=0,y=0)
         
-        self.prikaz_polja = tk.Canvas(self.okvir, height=200 , width=200)
-        self.prikaz_polja.place(x=0,y=0)
+        self.okno = okno
+        self.okvir = tk.Frame(okno, height=400, width=400)
+        self.okvir.grid(row=0,column=0)
+        
+        self.prikaz_polja = tk.Canvas(self.okvir,
+                                      height=200,
+                                      width=200)
+        self.prikaz_polja.grid(row=0,column=0)
 
         
-        okno.bind('<Key>', self.tipka)
-        
+        self.okno.bind('<Key>', self.tipka)
 
+        def gumb_nova_igra():
+            self.polje.korak = 0
+            self.polje.stevec = 0
+            self.polje.nova_igra()
+            self.narisi()
+            
+        gumb = tk.Button(self.okvir, text='NOVA IGRA',
+                         height=2, width=10,
+                         command=gumb_nova_igra)
+        gumb.grid(row = 0, column = 1)
+
+        self.stevilo_poskusov = tk.Label(self.okvir,
+                                         text='Število poskusov:')
+        self.stevilo_poskusov.grid(row = 1, column = 0)
+
+        self.stevilo_poskusov = tk.Label(self.okvir,
+                                         text=' 0 ',
+                                         bg='black',
+                                         fg='white',
+                                         relief='raised')
+        self.stevilo_poskusov.grid(row = 1, column = 1)
+
+        self.ime_igralca = tk.Label(self.okvir,text='Ime:')
+        self.ime_igralca.grid(row = 2, column = 0)
+        
+        self.ime_igralca = tk.Entry(self.okvir, bg='white')
+        self.ime_igralca.grid(row = 2, column = 1)
+
+        self.posodobi_poskuse()
+        
+     
+    def posodobi_poskuse(self):
+        self.stevilo_poskusov.configure(text=str(self.polje.stevec))
+
+    
     def narisi(self):
         
         for i in range(self.polje.visina):
             for j in range(self.polje.sirina):
-                self.prikaz_polja.create_polygon(20*j+2,20*i+2,20*j+20-2,20*i+2,20*j+20-2,20*i+20-2,20*j+2,20*i+20-2, fill='white')  
+                self.prikaz_polja.create_polygon(20*j+2,20*i+2,
+                                                 20*j+20-2,20*i+2,
+                                                 20*j+20-2,20*i+20-2,
+                                                 20*j+2,20*i+20-2,
+                                                 fill='white')  
 
-
-        
+        #Cilj
         i = 0
         j = 9
-        self.prikaz_polja.create_polygon(20*j+2,20*i+2,20*j+20-2,20*i+2,20*j+20-2,20*i+20-2,20*j+2,20*i+20-2,fill = 'red')
+        self.prikaz_polja.create_polygon(20*j+2,20*i+2,
+                                         20*j+20-2,20*i+2,
+                                         20*j+20-2,20*i+20-2,
+                                         20*j+2,20*i+20-2,
+                                         fill = 'red')
         self.prikaz_polja.create_text(20*j+10,20*i+10, text='C')
 
+        #Start
+        i = 9
+        j = 0
+        self.prikaz_polja.create_polygon(20*j+2,20*i+2,
+                                         20*j+20-2,20*i+2,
+                                         20*j+20-2,20*i+20-2,
+                                         20*j+2,20*i+20-2,
+                                         fill = 'red')        
+        self.prikaz_polja.create_text(10,20*9+10, text='S')
         
         for k in range(self.polje.korak):
             i, j = self.polje.resitev[k]
-            self.prikaz_polja.create_polygon(20*j+2,20*i+2,20*j+20-2,20*i+2,20*j+20-2,20*i+20-2,20*j+2,20*i+20-2,fill = 'red')
-            
-        self.prikaz_polja.create_text(10,20*9+10, text='S')
+            self.prikaz_polja.create_polygon(20*j+2, 20*i+2,
+                                             20*j+20-2, 20*i+2,
+                                             20*j+20-2, 20*i+20-2,
+                                             20*j+2, 20*i+20-2,
+                                             fill = 'red')
+        if self.polje.korak == 18:
+            self.shrani_rezultat()
+            self.prikaz_polja.delete('all')
+
+        self.posodobi_poskuse()
 
     def tipka(self, event):
         if event.keysym == 'Right':
@@ -41,11 +101,17 @@ class Pot:
             self.narisi()
         elif event.keysym == 'Up':
             self.polje.ugibaj(model.GOR)
-            self.narisi() 
+            self.narisi()
+
+    def shrani_rezultat(self):
+        with open('rezultati.txt', 'a') as datoteka:
+            print(self.ime_igralca.get(), str(self.polje.stevec),
+                  file=datoteka)
+
 
 
 okno = tk.Tk()
-a = Pot(okno)
-a.polje.nova_igra()
-a.narisi()
+igra = Pot(okno)
+igra.polje.nova_igra()
+igra.narisi()
 okno.mainloop()
